5179684840429fcf0ca445d2fdbc616f786c144c
Taufik Kurniawan <73595990+uPIKs92@users.noreply.github.com>
2021-03-08 20:20:58 +0700

    Update second-screen.blade.php

--- resources/views/radar/thomson/panel/second-screen.blade.php
+++ resources/views/radar/thomson/panel/second-screen.blade.php
@@ -504,7 +504,7 @@
                             </div>
                         </div>
                     </div> --}}
-                    <div class="row p-1 pb-2">
+                    {{-- <div class="row p-1 pb-2">
                         <div class="col-4 rows p-1">
                             <div class="wrapTable w-100 m-0">
                                 <div class="row" style="background-color: #333333; color: white; margin-right: 17px;">
@@ -824,6 +824,180 @@
                                         <div class="col-4 bg-secondary">Range</div>
                                         <div class="col pl-3">0</div>
                                     </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div> --}}
+                    <div class="row p-1 pb-2">
+                        <div class="col-sm-6 rows no-border p-1">
+                            <div class="wrapTable w-100 m-0">
+                                <div class="row" style="background-color: #333333; color: white; margin-right: 17px;">
+                                    <div class="col-4" style="font-size: 12px;">Variable</div>
+                                    <div class="col pl-3" style="font-size: 12px;">Value</div>
+                                    <div class="col pl-3" style="font-size: 12px;">Unit</div>
+                                </div>
+                                <div class="wrapCol text-white pb-0 h-Custom-sm-1">
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">Track ID</div>
+                                        <div class="col pl-3">0</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">Heading</div>
+                                        <div class="col pl-3">0</div>
+                                        <div class="col">Deg</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">Speed</div>
+                                        <div class="col pl-3">0</div>
+                                        <div class="col">Kts/Mach</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">ALT (primer)</div>
+                                        <div class="col pl-3">0</div>
+                                        <div class="col">ft</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">Quality</div>
+                                        <div class="col pl-3">0</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">Source</div>
+                                        <div class="col pl-3">0</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">MODE 1</div>
+                                        <div class="col pl-3">0</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">MODE 2</div>
+                                        <div class="col pl-3">0</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">MODE 3</div>
+                                        <div class="col pl-3">0</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">MODE C</div>
+                                        <div class="col pl-3">0</div>
+                                        <div class="col">nm</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">Callsign</div>
+                                        <div class="col pl-3">0</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">Bearing</div>
+                                        <div class="col pl-3">0</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">Range</div>
+                                        <div class="col pl-3">0</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">Georeff</div>
+                                        <div class="col pl-3">0</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 rows no-border p-1">
+                            <div class="wrapTable w-100 m-0">
+                                <div class="row" style="background-color: #333333; color: white; margin-right: 17px;">
+                                    <div class="col-4" style="font-size: 12px;">Variable</div>
+                                    <div class="col pl-3" style="font-size: 12px;">Value</div>
+                                    <div class="col pl-3" style="font-size: 12px;">Unit</div>
+                                </div>
+                                <div class="wrapCol text-white pb-0 h-Custom-sm-1">
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">Track ID</div>
+                                        <div class="col pl-3">0</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">Heading</div>
+                                        <div class="col pl-3">0</div>
+                                        <div class="col">Deg</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">Speed</div>
+                                        <div class="col pl-3">0</div>
+                                        <div class="col">Kts/Mach</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">ALT (primer)</div>
+                                        <div class="col pl-3">0</div>
+                                        <div class="col">ft</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">Quality</div>
+                                        <div class="col pl-3">0</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">Source</div>
+                                        <div class="col pl-3">0</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">MODE 1</div>
+                                        <div class="col pl-3">0</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">MODE 2</div>
+                                        <div class="col pl-3">0</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">MODE 3</div>
+                                        <div class="col pl-3">0</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">MODE C</div>
+                                        <div class="col pl-3">0</div>
+                                        <div class="col">nm</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">Callsign</div>
+                                        <div class="col pl-3">0</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">Bearing</div>
+                                        <div class="col pl-3">0</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">Range</div>
+                                        <div class="col pl-3">0</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4 bg-secondary">Georeff</div>
+                                        <div class="col pl-3">0</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 rows no-border p-1">
+                            <div class="wrapTable w-100 m-0">
+                                <div class="row" style="font-size: 12px; background-color: #333333; color: white; margin-right: 17px;">
+                                    <div class="col-0">No</div>
+                                    <div class="col-2">TKN</div>
+                                    <div class="col-1">HDG</div>
+                                    <div class="col-2">ALT</div>
+                                    <div class="col-1">F</div>
+                                    <div class="col-1">T</div>
+                                    <div class="col-2">Bearing</div>
+                                    <div class="col-2">Range</div>
+                                </div>
+                                <div class="wrapCol text-white pb-0 h-Custom-sm-1">
+                                    <table class="table table-bordered">
+                                        <tbody>
+                                            <tr>
+                                                <td>0</td>
+                                                <td>0</td>
+                                                <td>0</td>
+                                                <td>0</td>
+                                                <td>0</td>
+                                                <td>0</td>
+                                                <td>0</td>
+                                                <td>0</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
                         </div>
