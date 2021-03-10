fb7e74e7acbf623ed4d006a7d54bc857742f8a5b
Taufik Kurniawan <73595990+uPIKs92@users.noreply.github.com>
2021-03-10 13:34:38 +0700

    tccr grup 1

---
+++ resources/views/radar/thomson/panel/tccr.blade.php
@@ -0,0 +1,94 @@
+<div id="panel-TCCR" class="card no-border h-Custom4 p-1">
+    <div class="form-inline">
+        <div class="card-body rows">
+            <div class="row">
+                <div class="col-12">
+                    <span class="dot d-block"></span>
+                </div>
+                <div class="col-4">
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                </div>
+                <div class="col-8">
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                </div>
+                <div class="col-4 form-inline">
+                    <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                        <button type="button" class="btn btn-panel btn-none p-0 m-0">
+                            <span class="w-Custom12">Emerg stop</span>
+                        </button>
+                    </div>
+                    <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                        <label class="btn btn-panel btn-panel-none p-0 m-0">
+                            <input type="radio" name="controls" id="control-up-1">
+                        </label>
+                        <label class="btn btn-panel p-0 m-0">
+                            <input type="radio" name="controls" id="control-up-2">
+                            <span class="w-Custom12">ANT ON</span>
+                        </label>
+                        <label class="btn btn-panel p-0 m-0">
+                            <input type="radio" name="controls" id="control-up-3">
+                            <span class="w-Custom12">ANT OFF</span>
+                        </label>
+                    </div>
+                    <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                        <label class="btn btn-panel btn-panel-none p-0 m-0">
+                            <input type="radio" name="controls" id="control-down-1">
+                        </label>
+                        <label class="btn btn-panel p-0 m-0">
+                            <input type="radio" name="controls" id="control-down-2">
+                            <span class="w-Custom9">Tx OFF</span>
+                        </label>
+                        <span class="w-Custom7"></span>
+                        <label class="btn btn-panel p-0 m-0">
+                            <input type="radio" name="controls" id="control-down-3">
+                            <span class="w-Custom9">Tx ON</span>
+                        </label>
+                    </div>
+                    <div class="btn-group btn-group-toggle position-absolute" data-toggle="buttons" style="left: 82px; bottom: 2px;">
+                        <button type="button" class="btn btn-panel btn-none p-0 m-0">
+                            <span class="w-Custom12">STAND BY</span>
+                        </button>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="d-block">
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                    </div>
+                    <div class="d-block">
+                        <button type="button" class="btn btn-panel p-0 m-0">
+                            <span class="w-Custom12">Reset</span>
+                        </button>
+                        <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                            <button type="button" class="btn btn-panel btn-none p-0 m-0">
+                                <span class="w-Custom12">Hold</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                        <button type="button" class="btn btn-panel btn-none p-0 m-0">
+                            <span class="w-Custom12">Reduc power</span>
+                        </button>
+                        <button type="button" class="btn btn-panel btn-none p-0 m-0">
+                            <span class="w-Custom12">M T I OFF</span>
+                        </button>
+                        <button type="button" class="btn btn-panel btn-none p-0 m-0">
+                            <span class="w-Custom12">CAP OFF</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card-body rows">
+            tes
+        </div>
+    </div>
+</div>
