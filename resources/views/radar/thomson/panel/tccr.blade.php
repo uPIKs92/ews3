06d36d59622c8f5f8b8d5c91f6e4e9e0bfa516d7
Taufik Kurniawan <73595990+uPIKs92@users.noreply.github.com>
2021-03-12 17:04:35 +0700

    lanjutan

--- resources/views/radar/thomson/panel/tccr.blade.php
+++ resources/views/radar/thomson/panel/tccr.blade.php
@@ -1,5 +1,5 @@
 <div id="panel-TCCR" class="container p-0 h-Custom4">
-    <div class="row no-gutters text-uppercase text-white">
+    <div class="row no-gutters text-uppercase text-white pl-2 pt-2">
         <div class="col p-0" style="max-width: 572px;">
             <div class="border rounded-lg border-white" style="/*height: 345px;*/">
                 <div class="row no-gutters">
@@ -7,8 +7,8 @@
                         <span class="d-block tiny-2">primary radar</span>
                         <span class="d-block tiny-2">radar primaire</span>
                     </div>
-                    <div class="col-12 p-0">
-                        <span class="tiny font-weight-bold">local</span>
+                    <div class="col-12 p-0 pb-1">
+                        <span class="tiny font-weight-bold text-none">Tx LOCAL</span>
                         <span class="dot d-block m-auto"></span>
                     </div>
                     <div class="col-12 p-1">
@@ -19,18 +19,19 @@
                                     <span class="d-block tiny">status</span>
                                     <span class="d-block tiny">etat</span>
                                 </div>
-                                <div class="card-body pt-3 pb-2">
-                                    <span class="dot ml-3 mr-3"></span>
-                                    <span class="dot ml-3 mr-3"></span>
-                                    <span class="dot ml-3 mr-3"></span>
+                                <div class="card-body pt-4 pb-2" style="height: 85px;">
+                                    <span class="dot ml-3 mr-3 mt-1"></span>
+                                    <span class="dot ml-3 mr-3 mt-1"></span>
+                                    <span class="dot ml-3 mr-3 mt-1"></span>
                                 </div>
                             </div>
                             <div class="float-left card bg-transparent border border-white rounded ml-1"
                                 style="width: 356px;">
                                 <div class="card-body border-bottom border-white font-weight-bold">
-                                    <span class="d-block tiny">status</span>
-                                    <span class="d-block tiny">etat</span>
+                                    <span class="d-block tiny">performance reduction</span>
+                                    <span class="d-block tiny">performances reduites</span>
                                 </div>
+                                <div class="w-100"></div>
                                 <div class="card-body form-inline pt-3 pb-2">
                                     <div class="card-title w-20">
                                         <small class="tiny-sm">coverage</small>
@@ -235,13 +236,163 @@
                     </div>
                 </div>
             </div>
-            <div class="border rounded-lg border-white mt-2" style="height: 711px;">
+            <div class="border rounded-lg border-white mt-2" style="height: 611px;">
+                <div class="row no-gutters">
+                    <div class="col-12 pl-1 pt-1 pb-0">
+                        <span class="d-block tiny-2">primary radar operating controls</span>
+                        <span class="d-block tiny-2">commandes operationnelles radar primaire</span>
+                    </div>
+                </div>
 
             </div>
         </div>
-        <div class="col p-0" style="max-width: 379.5px;">
-            <div class="bg-info ml-2" style="height: 199px;"></div>
-            <div class="bg-success ml-2 mt-2" style="height: 857px;"></div>
+        <div class="col p-0" style="max-width: 371.5px;">
+            <div class="row no-gutters">
+                <div class="col-12 p-0 ml-2">
+                    <div class="clearfix">
+                        <div class="float-left card bg-transparent border border-white rounded mr-1"
+                            style="width: 173.75px;">
+                            <div class="card-body border-bottom border-white font-weight-bold" style="height: 31px;">
+                                <span class="d-block tiny m-2">alarm</span>
+                            </div>
+                            <div class="card-body p-2">
+                                <div class="w-100">
+                                    <button type="button" class="btn btn-panel p-0 m-0">
+                                        <span class="w-Custom12">STOP</span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="float-left card bg-transparent border border-white rounded ml-1"
+                            style="width: 173.75px;">
+                            <div class="card-body border-bottom border-white font-weight-bold">
+                                <span class="d-block tiny">local/remote</span>
+                                <span class="d-block tiny">local/distance</span>
+                            </div>
+                            <div class="card-body form-inline p-2">
+                                <div class="w-50">
+                                    <div class="btn-group btn-panel" data-toggle="buttons" style="margin-left: 11px;">
+                                        <label class="btn btn-panel p-0 m-0 active text-none">
+                                            <input type="radio">
+                                            <span class="w-Custom12">
+                                                Radar Head
+                                            </span>
+                                        </label>
+                                        <label class="btn btn-panel p-0 m-0 down text-none">
+                                            <input type="radio" checked>
+                                            <span class="w-Custom12">
+                                                Radar Head
+                                            </span>
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="w-50">
+                                    <div class="btn-group btn-panel" data-toggle="buttons" style="margin-left: 11px;">
+                                        <label class="btn btn-panel p-0 m-0 active">
+                                            <input type="radio">
+                                            <span class="w-Custom12">
+                                                MCRC
+                                            </span>
+                                        </label>
+                                        <label class="btn btn-panel p-0 m-0 down">
+                                            <input type="radio" checked>
+                                            <span class="w-Custom12">
+                                                MCRC
+                                            </span>
+                                        </label>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 p-0 mt-2 ml-2">
+                    <div class="clearfix">
+                        <div class="float-left card bg-transparent border border-white rounded" style="width: 107px;">
+                            <div class="card-body border-bottom border-white font-weight-bold mb-1">
+                                <span class="d-block tiny">line</span>
+                                <span class="d-block tiny">ligne</span>
+                            </div>
+                            <div class="card-body p-2" style="height: 70.25px;">
+                                <span class="dot d-block m-auto"></span>
+                            </div>
+                        </div>
+                        <div class="float-left form-inline card bg-transparent no-border" style="width: 66.75px;">
+                            <div class="btn-group btn-panel ml-2" data-toggle="buttons" style="margin-top: 27px;">
+                                <label class="btn btn-panel p-0 m-0 active text-none">
+                                    <input type="radio">
+                                    <span class="w-Custom12">
+                                        Lamp Test
+                                    </span>
+                                </label>
+                                <label class="btn btn-panel p-0 m-0 down text-none">
+                                    <input type="radio" checked>
+                                    <span class="w-Custom12">
+                                        Lamp Test
+                                    </span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 p-0 pt-1 mt-1 ml-2">
+                    <div class="clearfix">
+                        <div class="float-left card bg-transparent border border-white rounded mr-1"
+                            style="width: 173.75px;">
+                            <div class="row no-gutters p-0">
+                                <div class="col-12 form-inline p-1">
+                                    <div class="card-title pr-2 pt-3" style="width: 107px;">
+                                        <span class="dot d-block m-auto"></span>
+                                        <small class="d-block tiny-sm">jamming</small>
+                                        <small class="tiny-sm">brouillage</small>
+                                    </div>
+                                    <div class="btn-group-vertical">
+                                        <small class="tiny-sm m-auto pb-1">video</small>
+                                        <div class="btn-group btn-panel" data-toggle="buttons">
+                                            <label class="btn btn-panel p-0 m-0 active">
+                                                <input type="radio">
+                                                <span class="w-Custom12">
+                                                    CLV
+                                                </span>
+                                            </label>
+                                            <label class="btn btn-panel p-0 m-0 down">
+                                                <input type="radio" checked>
+                                                <span class="w-Custom12">
+                                                    CLV
+                                                </span>
+                                            </label>
+                                        </div>
+                                        <div class="btn-group btn-panel" data-toggle="buttons">
+                                            <label class="btn btn-panel p-0 m-0 active">
+                                                <input type="radio">
+                                                <span class="w-Custom12">
+                                                    RLV
+                                                </span>
+                                            </label>
+                                            <label class="btn btn-panel p-0 m-0 down">
+                                                <input type="radio" checked>
+                                                <span class="w-Custom12">
+                                                    RLV
+                                                </span>
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="float-left card bg-transparent border border-white rounded ml-1"
+                            style="width: 173.75px;">
+                            <div class="card-body border-bottom border-white font-weight-bold">
+                                <span class="d-block tiny">IFF</span>
+                            </div>
+                            <div class="card-body form-inline p-2">
+                                tes
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
